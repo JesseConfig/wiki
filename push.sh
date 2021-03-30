68cec478ad6fed403c8b7b318b65d1df17d68ab0
jesse <18736075818@.com>
2021-03-30 19:50:01 +0000

    2021-30-03

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wall "wiki Push start "$(date +%Y-%d-%m)
+wall "wiki Push start "$(date +"%Y-%d-%m %R")
 
 [ `pwd` == "/home/pi/wiki/wiki" ] || cd /home/pi/wiki/wiki
 [ `pwd` == "/home/pi/wiki/wiki" ] && git add . && git commit -m "`date +%Y-%d-%m`" && git push
 
-wall "wiki Push End "$(date +%Y-%d-%m)
+wall "wiki Push End "$(date +"%Y-%d-%m %R")
